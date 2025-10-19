65358dbadcfd1c3d868a718f92a18b994d85a5e4
sammysam254 <Sammyseth260@gmail.com>
2025-10-19 22:20:07 +0300

    Add components/Testimonials.tsx

---
+++ components/Testimonials.tsx
@@ -0,0 +1,23 @@
+export default function Testimonials() {
+  return (
+    <section className="bg-gray-200 py-8">
+      <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12 xl:p-24">
+        <h2 className="text-3xl font-bold">Testimonials</h2>
+        <ul>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+            </p>
+            <p>— John Doe</p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+            </p>
+            <p>— Jane Doe</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
