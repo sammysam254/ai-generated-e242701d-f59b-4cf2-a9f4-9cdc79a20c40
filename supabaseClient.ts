12cc53a99991435b481d7a35f92fa69aff70398b
sammysam254 <Sammyseth260@gmail.com>
2025-10-19 22:20:04 +0300

    Add supabaseClient.ts

---
+++ supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseSecret = process.env.SUPABASE_SECRET;
+
+export const supabaseClient = createClient(supabaseUrl, supabaseKey, supabaseSecret);
