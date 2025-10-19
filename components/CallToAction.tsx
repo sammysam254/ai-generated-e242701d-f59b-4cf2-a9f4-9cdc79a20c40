0ea49db6842c1837e4f78bad46cc5297a28056e8
sammysam254 <Sammyseth260@gmail.com>
2025-10-19 22:20:07 +0300

    Add components/CallToAction.tsx

---
+++ components/CallToAction.tsx
@@ -0,0 +1,15 @@
+export default function CallToAction() {
+  return (
+    <section className="bg-blue-500 py-8">
+      <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12 xl:p-24">
+        <h2 className="text-3xl font-bold text-white">Get Started Today!</h2>
+        <p className="text-lg text-white">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+        </p>
+        <button className="bg-white hover:bg-gray-100 text-blue-500 font-bold py-2 px-4 rounded">
+          Sign Up
+        </button>
+      </div>
+    </section>
+  );
+}
